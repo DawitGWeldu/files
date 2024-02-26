edb23b11205a425089436d5dcd73697822114bea
Dawit Getachew <gdawit35@gmail.com>
2024-02-26 17:33:36 +0300

    Completed course setup | Uploadthing, Mux

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "./logo";
+import { SidebarRoutes } from "./sidebar-routes";
+
+export const Sidebar = () => {
+    return (
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+            <div className="px-6 py-7">
+                <Logo />
+            </div>
+            <div className="flex flex-col w-full">
+                <SidebarRoutes />
+            </div>
+
+        </div>
+    )
+}
