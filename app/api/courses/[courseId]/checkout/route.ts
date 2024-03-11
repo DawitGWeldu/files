a54cf088f21105f8f3ac25fdefee40f5df4b25f2
Dawit Getachew <gdawit35@gmail.com>
2024-03-11 12:12:43 +0300

    m

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -55,7 +55,7 @@ export async function POST(
         last_name: "L",
         amount: `${course.price}`,
         tx_ref: tx_reference,
-        callback_url: "https://prototype-omega-cyan.vercel.app/api/verify-payment",
+        callback_url: "http://localhost:3000/api/verify-payment",
         return_url: return_url
       }
     }).then((res) => {
