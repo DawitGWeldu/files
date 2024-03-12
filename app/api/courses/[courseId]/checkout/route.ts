7c7f75424615fd028ea77cf5775235b9a5e9fdf3
Dawit Getachew <gdawit35@gmail.com>
2024-03-12 17:11:44 +0300

    fixed a bug?

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -48,7 +48,7 @@ export async function POST(
     method: "post",
     url: "https://api.chapa.co/v1/transaction/initialize",
     headers: {
-      "Authorization": "Bearer CHASECK_TEST-jlm8m3ZJiu9pNuvWiougRYAJ0moD3mqt"
+      "Authorization": `Bearer ${process.env.CHAPA_SECRET_KEY}`
     },
     data: {
       currency: "ETB",
@@ -56,8 +56,8 @@ export async function POST(
       last_name: "L",
       amount: 100,
       tx_ref: tx_reference,
-      // callback_url: callback_url,
-      // return_url: return_url
+      callback_url: callback_url,
+      return_url: return_url
     }
   })
 
@@ -72,86 +72,5 @@ export async function POST(
       userId: user.id!
     }
   })
-  // return NextResponse.json({ url: checkout_url });
-  return NextResponse.json({
-    return: process.env.NEXT_PUBLIC_VERCEL_URL,
-    callback: callback_url 
-  });
-
-  // try {
-
-  //   const user = await currentUser();
-
-  //   if (!user || !user.id || !user.phoneNumber) {
-  //     return new NextResponse("Unauthorized", { status: 401 });
-  //   }
-
-  //   // const course = await db.course.findUnique({
-  //   //   where: {
-  //   //     id: params.courseId,
-  //   //     isPublished: true,
-  //   //   }
-  //   // });
-
-  //   // const purchase = await db.purchase.findUnique({
-  //   //   where: {
-  //   //     userId_courseId: {
-  //   //       userId: user.id,
-  //   //       courseId: params.courseId
-  //   //     }
-  //   //   }
-  //   // });
-
-  //   // if (purchase) {
-  //   //   return new NextResponse("Already purchased", { status: 400 });
-  //   // }
-
-  //   // if (!course) {
-  //   //   return new NextResponse("Not found", { status: 404 });
-  //   // }
-  //   // const tx_reference = uuidv4();;
-  //   // const return_url = `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}`;
-  //   // // const callback_url = `${process.env.NEXT_PUBLIC_APP_URL}/api/verify-payment`;
-
-  //   // let checkout_url = null;
-  //   // const res = await axios({
-  //   //   method: "post",
-  //   //   url: "https://api.chapa.co/v1/transaction/initialize",
-  //   //   headers: {
-  //   //     "Authorization": "Bearer " + process.env.CHAPA_SECRET_KEY
-  //   //   },
-  //   //   data: {
-  //   //     currency: "ETB",
-  //   //     first_name: "F",
-  //   //     last_name: "L",
-  //   //     amount: `${course.price}`,
-  //   //     tx_ref: tx_reference,
-  //   //     // callback_url: callback_url,
-  //   //     return_url: return_url
-  //   //   }
-  //   // })
-
-  //   //   console.log("[CHECKOUT URL]: ", JSON.stringify(res.data.data.checkout_url))
-  //   //   if(res.data.status == "success") {
-  //   //   checkout_url = res.data.data.checkout_url
-  //   //   }
-  //   // // }).catch((err) => {
-  //   // //   // console.log("[CHECKOUT ERROR]: ", JSON.stringify(err))
-  //   // //   throw new Error("Checkout Failed");
-  //   // // });
-
-  //   // await db.chapaTransaction.create({
-  //   //   data: {
-  //   //     courseId: course.id,
-  //   //     tx_ref: tx_reference,
-  //   //     userId: user.id
-  //   //   }
-  //   // })
-  //   // return NextResponse.json({ url: checkout_url});
-  //   return NextResponse.json({ url: "https://github.com/DawitGWeldu/Next_LMS/commit/8605fa6213216506eb418ede4aca255c3f25125a"});
-  // } catch (error) {
-  //   console.log("[COURSE_ID_CHECKOUT]", error);
-  //   // return new NextResponse("Internal Error", { status: 500 })
-  //   throw new Error("Checkout Failed");
-  // }
+  return NextResponse.json({ url: checkout_url });
 }
