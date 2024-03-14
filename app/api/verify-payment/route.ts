6935d5138bfb8826cdd565262b943a79ed9e60aa
Dawit Getachew <gdawit35@gmail.com>
2024-03-14 12:03:14 +0300

    Cors vercel.json

--- app/api/verify-payment/route.ts
+++ app/api/verify-payment/route.ts
@@ -1,40 +1,40 @@
 import { db } from "@/lib/db";
 import { ChapaTransaction, Course } from "@prisma/client"
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
     req: NextRequest) {
     const searchParams  = req.nextUrl.searchParams;
-    const status = searchParams.get("status");
+    const status = searchParams.has("status");
     console.log("[CALLBACK RAN]: [QUERY PARAMS]: ", JSON.stringify(req.url))
 
     let tx_ref: string = "";
-    if (status == "success") {
+    // if (status == "success") {
 
-        tx_ref = searchParams.get('trx_ref')!;
-        try {
-            const transaction = await db.chapaTransaction.findFirst({
-                where: {
-                    tx_ref: searchParams.get('trx_ref')!,
-                    status: 'PENDING'
-                }
-            })
-            // console.log(JSON.stringify(transaction))
+    //     tx_ref = searchParams.get('trx_ref')!;
+    //     try {
+    //         const transaction = await db.chapaTransaction.findFirst({
+    //             where: {
+    //                 tx_ref: searchParams.get('trx_ref')!,
+    //                 status: 'PENDING'
+    //             }
+    //         })
+    //         // console.log(JSON.stringify(transaction))
 
-            await db.purchase.create({
-                data: {
-                    courseId: transaction!.courseId,
-                    userId: transaction!.userId,
-                }
-            });
-            console.log("[CALLBACK RAN]: Success")
+    //         await db.purchase.create({
+    //             data: {
+    //                 courseId: transaction!.courseId,
+    //                 userId: transaction!.userId,
+    //             }
+    //         });
+    //         console.log("[CALLBACK RAN]: Success")
 
-            return Response.json({ status: "Transaction Success" });
-        } catch (error) {
-            throw new Error("Transaction Data not found")
-        }
+    //         return Response.json({ status: "Transaction Success" });
+    //     } catch (error) {
+    //         throw new Error("Transaction Data not found")
+    //     }
 
-    }
+    // }
     console.log("[CALLBACK RAN]: Error")
-    return Response.json({ status: "Transaction Not Success" });
+    return new NextResponse("Transaction Not Success",{ status: 200 });
 }
--- next.config.mjs
+++ next.config.mjs
@@ -14,10 +14,11 @@ const nextConfig = {
         {
           source: '/api/:path*', // Matches all API routes
           headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: 'Access-Control-Allow-Origin', value: '*' }, // Replace with Chapa's exact domain
-            { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' }, // Adjust methods based on your API routes
-            { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }, // Adjust headers if needed
-          ],
+            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
         },
     ],
 };
